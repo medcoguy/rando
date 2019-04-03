7434f014f5881a4bfcaa4853f8549b6e5b8c5594
medcoguy <drewbrooksbank@gmail.com>
2019-04-03 09:55:14 -0500

    Updates

---
+++ precache-manifest.98f59ac9e18521e589ed9163d30be9f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17f52c1138cd484b60d7",
+    "url": "/medcoguy/Clicky/static/js/runtime~main.86e90fc0.js"
+  },
+  {
+    "revision": "f9f019f9f0b86e3563f0",
+    "url": "/medcoguy/Clicky/static/js/main.f4d889d8.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/medcoguy/Clicky/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "f9f019f9f0b86e3563f0",
+    "url": "/medcoguy/Clicky/static/css/main.0e18c572.chunk.css"
+  },
+  {
+    "revision": "b4548aa869014898ca2674531aa2280e",
+    "url": "/medcoguy/Clicky/index.html"
+  }
+];
